2220127ac7389a3f65393f26a8049aaf427e7172
Carlos Soriano <carlos.soriano@boeing.com>
2020-01-10 14:24:39 -0600

    initial commit

---
+++ FizzBuzz.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace rht
+{
+    public class FizzBuzz
+    {
+      public static void GenerateBuzz(int start, int end)
+      {
+        foreach (uint n in Enumerable.Range(start, end))
+        {
+
+        }
+      }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace rht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // FizzBuzz.GenerateBuzz(1,2);
+          string test = "test";
+          Logger.WriteFile(test);
+        }
+    }
+}
